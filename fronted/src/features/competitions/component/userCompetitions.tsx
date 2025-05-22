d4811c65406676e129562632d705b307ab45b1ee
tzipi-devloper <t0504110765@gmail.com>
2025-05-23 02:08:41 +0300

    fixed feture/show-my-competitions

--- fronted/src/features/competitions/component/userCompetitions.tsx
+++ fronted/src/features/competitions/component/userCompetitions.tsx
@@ -1,42 +1,4 @@
-// import React from "react";
-// import { useGetUserCompetitionsByUserIdQuery } from "../competitionsAPI";
-// import { CompetitionItem } from "../competitionsTypes";
 
-// interface UserCompetitionsProps {
-//   userId: string;
-// }
-
-// const UserCompetitions: React.FC<UserCompetitionsProps> = ({ userId }) => {
-//   const { data: competitions, error, isLoading } = useGetUserCompetitionsByUserIdQuery(userId);
-
-//   if (isLoading) return <div>Loading competitions...</div>;
-//   if (error) return <div>Error loading competitions</div>;
-//   if (!competitions || competitions.length === 0) return <div>No competitions found</div>;
-
-//   return (
-//     <div>
-//       <h2>User Competitions</h2>
-//       <ul>
-//         {competitions.map((comp: CompetitionItem) => (
-//           <li key={comp._id}>
-//             <strong>Category:</strong> {comp.category} <br />
-//             <strong>Rating:</strong> {comp.rating} <br />
-//             {/* אפשר להוסיף עוד פרטים לפי צורך */}
-//             {comp.fileUrl && (
-//               <div>
-//                 <a href={comp.fileUrl} target="_blank" rel="noopener noreferrer">
-//                   View File
-//                 </a>
-//               </div>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default UserCompetitions;
 import React from 'react';
 import {
   useGetUserCompetitionsByUserIdQuery
